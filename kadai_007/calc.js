0781721e570f922aaec92f9db93dcbe607c08f39
kaeshi-hiki <kanei.9921.hak@gmail.com>
2025-10-15 17:23:30 +0900

    second commit

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,15 +1,11 @@
-let num = Math.floor(Math.random() * 100);
-switch (0) {
-  case num % 15:
+let num = Math.floor(Math.random() * 100)+1;
+
+if (num % 3 === 0 && num % 5 === 0) {
     console.log("3と5の倍数です");
-    break;
-  case num % 3:
+} else if (num % 3 === 0) {
     console.log("3の倍数です");
-    break;
-  case num % 5:
+} else if (num % 5 === 0) {
     console.log("5の倍数です");
-    break;
-  default:
+} else {
     console.log(num);
-    break;
 }
